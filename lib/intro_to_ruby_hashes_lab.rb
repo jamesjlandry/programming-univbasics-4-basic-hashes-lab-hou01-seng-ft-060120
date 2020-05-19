a4c2cd9b13a5a5287be8e0e59694f040012295b9
James Landry <james.j.landry@outlook.com>
2020-05-19 19:47:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,10 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hashmiester = {
+    name: "bob",
+    occupation: "builder"
+  }
 end
 
 def pioneer
